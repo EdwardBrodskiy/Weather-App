55271f0e055772d03b8cf555d7af71da9629f841
Kacper <ec19315@qmul.ac.uk>
2021-03-05 16:53:41 +0000

    changed todo

--- src/routes/week/index.js
+++ src/routes/week/index.js
@@ -2,7 +2,7 @@ import { h } from 'preact'
 import style from './style.css'
 
 const Week = () => {
-  return <div class={style.week}>TODO</div>
+  return <div class={style.week}>TODO Kacper's Job</div>
 }
 
 export default Week
