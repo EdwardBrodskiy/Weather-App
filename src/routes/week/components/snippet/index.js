3a182a12abd27fd5b3fab33ca846a44d95db1f1f
Kacper <ec19315@qmul.ac.uk>
2021-03-06 14:07:32 +0000

    first attempt to copy forecast

---
+++ src/routes/week/components/snippet/index.js
@@ -0,0 +1,17 @@
+import { h } from 'preact'
+import Moment from 'moment'
+import capitalize from 'capitalize'
+import style from './style.css'
+import icon_mapping from '../../../../mapping.json'
+
+export const Snippet = ({ time, description, state, temp }) => {
+  return (
+    <div class={style.snippet}>
+      <div class={style.text}>
+        <p>{Moment(time).format('LT')} - {temp}°C</p>
+        <p>{capitalize(description)}</p>
+      </div>
+      <i class={`wi ${icon_mapping[state]}`} />
+    </div>
+  )
+}
--- src/routes/week/index.js
+++ src/routes/week/index.js
@@ -1,8 +1,24 @@
 import { h } from 'preact'
 import style from './style.css'
+import { Snippet } from './components/snippet'
 
-const Week = () => {
-  return <div class={style.week}>TODO Kacper's Job</div>
+const Week = (props) => {
+  const snippets = props.data.list.slice(1, 5).map((item, index) => (
+    <div key={index}>
+      <Snippet
+        time={item.dt_txt}
+        description={item.weather[0].description}
+        state={item.weather[0].main}
+        temp={Math.round(item.main.temp - 273.15)}
+      />
+      <hr />
+    </div>
+  ))
+  return (
+    <div class={style.week}>
+      <p>TODO</p>
+    </div>
+  )
 }
 
 export default Week
