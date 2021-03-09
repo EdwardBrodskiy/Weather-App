553aaabd4e17cd7a7dce0f979427efc81f1e82ae
EdwardBrodskiy <brodskiedward@gmail.com>
2021-03-09 13:55:24 +0000

    added dynamic location

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import { h } from 'preact'
 import { useState, useEffect } from 'preact/compat'
 import { Router } from 'preact-router'
 import { createHashHistory } from 'history'
-import { apiEndPoint } from '../config.json'
+import { apiURL, appId } from '../config.json'
 
 import Header from './header'
 import { WeatherInfo } from './weather_info'
@@ -18,17 +18,23 @@ const App = () => {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    (async () => {
-      const response = await fetch(apiEndPoint)
-      if (!response.ok) {
-        throw Error(response.statusText)
-      }
-      const result = await response.json()
-      setIsLoaded(true)
-      setData(result)
-    })().catch((err) => {
-      setError(err)
-    })
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        console.log(`lon ${lon} lat ${lat}`)
+        get_weather(lat, lon)
+          .then(result => {
+            setIsLoaded(true)
+            setData(result)
+          }).catch((err) => {
+            setError(err)
+          })
+      })
+    } else {
+      setError(Error('Geolocation required'))
+    }
+
   }, [])
 
   if (error.message) {
@@ -52,4 +58,19 @@ const App = () => {
     </div>
   )
 }
+
+const get_weather = async (lat, lon) => {
+  const url = `${apiURL}lat=${lat}&lon=${lon}&appid=${appId}`
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw Error(response.statusText)
+  }
+  const result = await response.json()
+
+  return Promise.resolve(result)
+}
+
 export default App
+
+// https://api.openweathermap.org/data/2.5/forecast?lat=51.4809597&lon=-0.11796979999999999&appid=f3d0541df7d71acb69f6716e4de3bcc2
+//https://api.openweathermap.org/data/2.5/forecast?lat=52.520007&lon=13.404954&appid=f3d0541df7d71acb69f6716e4de3bcc2
