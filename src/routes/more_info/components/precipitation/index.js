25b25641b17dd25d0aa70bbaafc5ad30f44b8a3e
Rayyan Badshah <rayyanbadshah@Rayyans-MBP.lan>
2021-03-15 10:59:50 +0000

    changes made to files

---
+++ src/routes/more_info/components/precipitation/index.js
@@ -0,0 +1,9 @@
+import { h } from 'preact'
+// eslint-disable-next-line no-unused-vars
+import style from './style.css'
+
+export const Precipitacion = ({value}) => {
+    <div class={style.precipitacion}>
+        <p>{value}</p>
+    </div>
+}
