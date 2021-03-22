db5bb9e8f6ff6da5826fd9ad557ffd47ae97e3c2
EdwardBrodskiy <brodskiedward@gmail.com>
2021-03-22 15:58:45 +0000

    added extensive theming

--- src/components/themer/index.js
+++ src/components/themer/index.js
@@ -5,30 +5,42 @@ import style from './style.css'
 const themes = {
   default: {
     '--background-color': 'linear-gradient(160deg, rgba(73, 9, 121, 1) 23%, rgba(114, 137, 218, 1) 100%)',
-    '--text-color': 'white'
+    '--text-color': '#ffffff',
+    '--alt-text-color': '#ffffff',
+    '--primary': '#7289da',
+    '--secondary': '#99aab5',
+    '--background-0': '#23272a',
+    '--background-1': '#2c2f33',
   },
   high_contrast: {
     '--background-color': '#23272a',
-    '--text-color': 'white'
+    '--text-color': '#ffffff',
+    '--alt-text-color': '#23272a',
+    '--primary': '#7289da',
+    '--secondary': '#99aab5',
+    '--background-0': '#23272a',
+    '--background-1': '#2c2f33'
   }
 }
 
 const Themer = () => {
   const [theme, setTheme] = useState('default')
 
-  console.log(theme)
-  for (const property in themes[theme]) {
-    document.body.style.setProperty(property, themes[theme][property])
-  }
-
   return (
     <div class={style.themer}>
       <button onClick={() => {
-        console.log('hi')
-        setTheme(theme === 'default' ? 'high_contrast' : 'default')
+        const new_theme = theme === 'default' ? 'high_contrast' : 'default'
+        update_style(themes[new_theme])
+        setTheme(new_theme)
       }}>High Contrast</button>
     </div>
   )
+}
+
+const update_style = theme => {
+  for (const property in theme) {
+    document.body.style.setProperty(property, theme[property])
+  }
 }
 
 export default Themer
