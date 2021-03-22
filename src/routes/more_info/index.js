532023ce19de862e50ccbea317a2c51ac89a3c95
Rayyan Badshah <rayyanbadshah@Rayyans-MBP.lan>
2021-03-22 16:11:02 +0000

    added the api call for precipitaion

--- src/routes/more_info/index.js
+++ src/routes/more_info/index.js
@@ -4,6 +4,29 @@ import style from './style.css'
 import { Snippet } from './components/snippet'
 import { Wind } from './components/wind'
 import { Precipitacion } from './components/precipitation'
+import { One_Call} from '../../config.json'
+import { useEffect } from 'preact/hooks'
+/*
+const [pData, setPData] = useState({});
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition(postion =>
+    {
+      const lat = postion.coords.latitude
+      const lon = postion.coords.longitude
+      getPrecipData(lat,lon)
+       .then(result =>{
+         setPData(result)
+       })
+    })
+},[])
+
+const getPrecipData = async (lat,lon) => {
+  const url = `${One_Call.apiURL}lat=${lat}&lon=${lon}&appid=${One_Call.appId}`
+  const response = await fetch(url)
+  const result = await response.json()
+  return Promise.resolve(result)
+}
+*/
 
 const MoreInfo = (props) => {
   const current = props.data.list[0]
@@ -16,11 +39,13 @@ const MoreInfo = (props) => {
         <hr />
         <Snippet title='Humidity' content={`${current.main.humidity}%`} />
         <hr />
-        <Precipitacion title ='Precipitation' />
+        <Precipitacion title ='Precipitation' content={`${pData.minutely.precipitacion}`} />
         <hr />
         <Wind direction={current.wind.deg} speed={current.wind.speed} />
       </ul>
     </div>)
 }
+
+
 
 export default MoreInfo
