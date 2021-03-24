9bf5428d374ee91e58eedcefe58ed2b2ce99db44
Mohammed Al-Dubooni <m.al-dubooni@se19.qmul.ac.uk>
2021-03-24 17:09:54 +0000

    update forecast and week

--- src/routes/week/components/snippet/index.js
+++ src/routes/week/components/snippet/index.js
@@ -2,16 +2,20 @@ import { h } from 'preact'
 import Moment from 'moment'
 import capitalize from 'capitalize'
 import style from './style.css'
-import icon_mapping from '../../../../mapping.json'
+import { convert_to_icon } from '../../../../utils'
 
 export const Snippet = ({ time, description, state, temp }) => {
+
+  const current_state = current.weather[0].description
+  const icon_class = convert_to_icon(current_state, data.city.sunrise, data.city.sunset)
+
   return (
     <div class={style.snippet}>
       <div class={style.text}>
         <p class={style.day}>{Moment(time).format('dddd')} {temp}°C</p>
         <p>{capitalize(description)}</p>
       </div>
-      <i class={`wi ${icon_mapping[state]}`} />
+      <i class={`wi ${icon_class}`} />
     </div>
   )
 }
