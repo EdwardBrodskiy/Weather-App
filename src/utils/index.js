8aab6c4c9460eb4148451471672597adde22c52d
Mohammed Al-Dubooni <m.al-dubooni@se19.qmul.ac.uk>
2021-03-24 16:02:09 +0000

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,3 @@
-import icon_mapping from '../mapping.json'
-import current_state from '../components/weather_info/index.js'
 
 export const convert_to_icon = (description) => {
   var d = new Date()
@@ -7,13 +5,12 @@ export const convert_to_icon = (description) => {
   sunrise_hour = getHours(data.city.sunrise())
   sunset_hour = getHours(data.city.sunset())
   if(current_hour>0 && current_hour<=sunrise_hour) {
-    return 'night ${icon_mapping[current_state]'
+    return icon_mapping.night[description]
   }
   else if(current_hour>sunrise_hour && current_hour<=sunset_hour) {
-    return 'day ${icon_mapping[current_state]'
+    return icon_mapping.day[description]
   }
   else if(current_hour>sunset_hour && current_hour<=0) {
-    return 'neutral ${icon_mapping[current_state]'
+    return icon_mapping.neutral[description]
   }
-  return <css_icon_name>
 }
